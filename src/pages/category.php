e5cc9568c08bcadfc9bd016ea62ec60f1cfc8c12
yaroslav nakonechniy <yarosalv.n@open-tag.com>
2021-10-08 22:21:08 +0300

    Add file: category,home,index,blog,data,contact-us,paga,.htaccess

---
+++ src/pages/category.php
@@ -0,0 +1,15 @@
+<section title="Products">
+    <h1><?= $data['name'] ?></h1>
+    <div class="product-list">
+        <?php foreach (catalogGetCategoryBlog($data['category_id']) as $blog) : ?>
+            <div class="product">
+                <a href="/<?= $blog['url'] ?>" title="<?= $blog['name'] ?>">
+                    <img src="/product-placeholder.png" alt="<?= $blog['name'] ?>" width="200"/>
+                </a>
+                <a href="/<?= $blog['url'] ?>" title="<?= $blog['name'] ?>"><?= $blog['name'] ?></a>
+                <span>data: <?= $blog['data'] ?></span>
+                <button type="button">Add To Cart</button>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
